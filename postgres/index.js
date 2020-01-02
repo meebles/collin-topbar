85442ea682425aa1ee2ae22938662af7d1e69b1d
Collin Snyder <collinjacobsnyder@gmail.com>
2020-01-02 16:36:17 -0600

    Add getKnid route for testing

--- postgres/index.js
+++ postgres/index.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "collinsnyder",
-  host: "localhost",
-  database: "meeblestopbar",
-  password: "postgres",
+  user: "collin",
+  host: "ec2-18-219-128-13.us-east-2.compute.amazonaws.com",
+  database: "collin",
+  password: "hau boue",
   port: 5432
 });
 
@@ -20,5 +20,10 @@ module.exports = {
         "SELECT * FROM products WHERE real_name = 'Handskar' AND id > 9000000 LIMIT 10"
       )
       .then(data => callback(data.rows))
+      .catch(err => callback(err)),
+  getKnid: callback =>
+    pool
+      .query("SELECT * FROM products WHERE real_name = 'Vermicious Knid'")
+      .then(data => callback(data))
       .catch(err => callback(err))
 };
